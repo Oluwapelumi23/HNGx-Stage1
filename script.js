7c225d5892aedd6001f57ad1a958c6cace1bf6ce
Oluwapelumi23 <129471012+Oluwapelumi23@users.noreply.github.com>
2023-09-12 04:43:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 "use strict";
+
+// document.querySelector('[data-testid="slackUserName"]').innerHTML =
+//   "Victor Oluwapelumi";
+
 document.querySelector('[data-testid="myTrack"]').innerHTML = "Frontend";
 
 const getCurrentDay = () => {
@@ -16,40 +20,21 @@ const getCurrentDay = () => {
   return dayName;
 };
 
+const getCurrentUTCTime = () => {
+  const utcMilliseconds = Date.now();
+  return utcMilliseconds;
+};
+
 const update = () => {
   const currentDayOfTheWeek = document.querySelector(
     '[data-testid="currentDayOfTheWeek"]'
   );
+  const currentUTCTimeElement = document.querySelector(
+    '[data-testid = "currentUTCTime"]'
+  );
 
   currentDayOfTheWeek.textContent = `${getCurrentDay()}`;
+  currentUTCTimeElement.textContent = `UTCTime: ${getCurrentUTCTime()} milliseconds`;
 };
 
 window.onload = update;
-
-function updateUTCTime() {
-  const today = new Date();
-  let hours = today.getHours();
-  let minutes = today.getMinutes();
-  let seconds = today.getSeconds();
-  let milliseconds = today.getMilliseconds();
-
-  hours = checkTime(hours);
-  minutes = checkTime(minutes);
-  seconds = checkTime(seconds);
-
-  const currentUTCTime =
-    hours + "" + minutes + "" + seconds + "" + milliseconds;
-
-  const time = document.querySelector('[data-testid="currentUTCTime"]');
-
-  time.innerHTML = currentUTCTime;
-}
-
-function checkTime(i) {
-  if (i < 10) {
-    i = "0" + i;
-  }
-  return i;
-}
-
-updateUTCTime();
