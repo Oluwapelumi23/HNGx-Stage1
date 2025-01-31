0700714b98af79ce8e7f1b5d3c41540da81fddbd
Oluwapelumi23 <129471012+Oluwapelumi23@users.noreply.github.com>
2025-01-31 17:46:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,13 @@
 "use strict";
 
-// document.querySelector('[data-testid="slackUserName"]').innerHTML =
-//   "Victor Oluwapelumi";
+const utcT = document.querySelector(".UTC-Time");
+const day = new Date();
 
-document.querySelector('[data-testid="myTrack"]').innerHTML = "Frontend";
+const UTChours = day.getUTCHours();
+const UTCminutes = day.getUTCMinutes();
+const UTCseconds = day.getUTCSeconds();
 
-const getCurrentDay = () => {
-  const weekDay = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  const today = new Date();
-  const dayName = weekDay[today.getDay()];
-  return dayName;
-};
+const time = `${UTChours}:${UTCminutes}:${UTCseconds}`;
+console.log(time);
 
-const getCurrentUTCTime = () => {
-  const utcMilliseconds = Date.now();
-  return utcMilliseconds;
-};
-
-const update = () => {
-  const currentDayOfTheWeek = document.querySelector(
-    '[data-testid="currentDayOfTheWeek"]'
-  );
-  const currentUTCTimeElement = document.querySelector(
-    '[data-testid = "currentUTCTime"]'
-  );
-
-  currentDayOfTheWeek.textContent = `${getCurrentDay()}`;
-  currentUTCTimeElement.textContent = `${getCurrentUTCTime()} milliseconds`;
-};
-
-window.onload = update;
+utcT.innerHTML = time;
